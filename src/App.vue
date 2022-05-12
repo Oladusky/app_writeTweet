2d6bbb0bb2b287fec03dbd7de53bba9a3d736ddd
Olga Kinah <oladuska2300@gmail.com>
2022-05-12 13:11:50 +0300

    all app

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-context">
+      <div class="view">
+        <div class="container">
+          <Form @onSubmit="handleSubmit" />
+          <List :items="items" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import {ref} from 'vue'
+import List from "@/components/List.vue";
+import Form from "@/components/Form.vue";
+
+export default {
+  components: { Form, List },
+  setup() {
+    const items = ref([
+      {
+        id: 1,
+        avatar: `https://avatars.dicebear.com/api/croodles/1.svg`,
+        body: "hello",
+        date: new Date(Date.now()).toLocaleString(),
+        likes: 0,
+      },
+    ])
+
+    const handleSubmit = (item) => items.value.push(item);
+    
+
+    return {
+      items,
+      handleSubmit
+    };
+  },
+};
+
+// export default {
+//   components: { Form, List },
+//   data() {
+//     return {
+//       items: [
+//         {
+//           id: 1,
+//           avatar: `https://avatars.dicebear.com/api/croodles/1.svg`,
+//           body: "hello",
+//           date: new Date(Date.now()).toLocaleString(),
+//           likes: 12,
+//         },
+//       ],
+//     };
+//   },
+//   methods: {
+//     handleSubmit(item) {
+//       console.log(item);
+//       this.items.push(item);
+//     },
+//   },
+// };
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <textarea v-model="body" required placeholder="Type tweet here"></textarea>
+    <br />
+    <button class="btn btnTweet" type="submit">Post Tweet</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  emits: ["onSubmit"],
+  setup(_, { emit }) {
+    const body = ref("");
+    const onSubmit = () => {
+      emit("onSubmit", {
+        id: Math.round(Math.random() * 30),
+        avatar: `https://avatars.dicebear.com/api/croodles/${Date.now()}.svg`,
+        body: body.value,
+        date: new Date(Date.now()).toLocaleString(),
+        likes: 0,
+      });
+      body.value = "";
+    };
+
+    return { body, onSubmit };
+  }
+}
+// export default {
+//   data() {
+//     body: "";
+//   },
+//   methods: {
+//     onSubmit() {
+//       this.$emit("onSubmit", {
+//         id: Math.round(Math.random() * 30),
+//         avatar: `https://avatars.dicebear.com/api/croodles/${Date.now()}.svg`,
+//         body: this.item,
+//         date: new Date(Date.now()).toLocaleString(),
+//         likes: 0,
+//       });
+//       this.item = "";
+//     },
+//   },
+// };
+</script>
